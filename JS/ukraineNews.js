742fdc62bdf693bcc4e6d1510e1d8d675829111e
odinserhii <playmarket06092013@gmail.com>
2025-12-23 11:11:57 +0000

    Add site structure and styles

--- JS/ukraineNews.js
+++ JS/ukraineNews.js
@@ -1,4 +1,4 @@
-const container = document.getElementById("ua-news");
+const list = document.getElementById("ukraine-news-list");
 
 const RSS_URL = "https://www.ukrinform.ua/rss/block-lastnews";
 const PROXY_URL = "https://corsproxy.io/?" + encodeURIComponent(RSS_URL);
@@ -9,23 +9,33 @@ fetch(PROXY_URL)
     const parser = new DOMParser();
     const data = parser.parseFromString(xml, "text/xml");
 
-    const item = data.querySelector("item");
+    const items = data.querySelectorAll("item");
 
-    if (!item) {
-      container.innerText = "Новин немає";
+    if (!items.length) {
+      list.innerHTML = "<li>Новин немає</li>";
       return;
     }
 
-    const title = item.querySelector("title").textContent;
-    const link = item.querySelector("link").textContent;
+    list.innerHTML = ""; // очищаємо список
 
-    container.innerHTML = `
-      <a href="${link}" target="_blank" rel="noopener">
-        ${title}
-      </a>
-    `;
+    // беремо ПЕРШІ 5 новин
+    items.forEach((item, index) => {
+      if (index >= 8) return;
+
+      const title = item.querySelector("title")?.textContent;
+      const link = item.querySelector("link")?.textContent;
+
+      const li = document.createElement("li");
+      li.innerHTML = `
+        <a href="${link}" target="_blank" rel="noopener">
+          ${title}
+        </a>
+      `;
+
+      list.appendChild(li);
+    });
   })
   .catch(error => {
     console.error(error);
-    container.innerText = "Не вдалося завантажити новину";
+    list.innerHTML = "<li>Не вдалося завантажити новини</li>";
   });
